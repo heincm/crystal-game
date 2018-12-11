8a565f694243aafdcb15995f11c8952c6c4c4de1
Christopher Hein <chrishein@Christophers-MacBook-Pro.local>
2018-12-11 18:29:11 -0500

    Game functions as it should

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -25,6 +25,7 @@ var numberOptions
 
 // Next we create a for loop to create crystals for every numberOption.
 function createNewCrystals() {
+    numberOptions = getRandomInts();
     for (var i = 0; i < numberOptions.length; i++) {
 
         // For each iteration, we will create an imageCrystal
@@ -50,47 +51,43 @@ function createNewCrystals() {
 function resetGame() {
     numberOptions = getRandomInts();
     // This generates the random number for the score that needs to be achieved
-    targetNumber = 9//getRandomInt(19, 120);
+    targetNumber = getRandomInt(19, 120);
     $("#targetNumber").html(targetNumber);
     createNewCrystals();
     counter = 0;
     $("#counter").html(counter)
     console.log(numberOptions);
-    $(document).ready();
-}
+    startGame();
+};
 
+
+
+function startGame(){
+$(".crystal-image").on("click", function (){
+
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+    // We then add the crystalValue to the user's "counter" which is a global variable.
+    // Every click, from every crystal adds to the global counter.
+    counter += crystalValue;
+
+    // All of the same game win-lose logic applies. So the rest remains unchanged.
+    $("#counter").html(counter);
+
+    if (counter === targetNumber) {
+        wins++;
+        $("#wins").html(wins);
+        $(".crystal-image").remove();
+        resetGame();
+    }
+
+    else if (counter > targetNumber) {
+        losses++;
+        $("#losses").html(losses);
+        $(".crystal-image").remove();
+        resetGame();
+    }
+
+})};
 resetGame();
-// This time, our click event applies to every single crystal on the page. Not just one.
-$(document).ready(function () {
-    $(".crystal-image").on("click", function () {
-
-        // Determining the crystal's value requires us to extract the value from the data attribute.
-        // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-        // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-        // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-
-        var crystalValue = ($(this).attr("data-crystalvalue"));
-        crystalValue = parseInt(crystalValue);
-        // We then add the crystalValue to the user's "counter" which is a global variable.
-        // Every click, from every crystal adds to the global counter.
-        counter += crystalValue;
-
-        // All of the same game win-lose logic applies. So the rest remains unchanged.
-        $("#counter").html(counter);
-
-        if (counter === targetNumber) {
-            wins++;
-            $("#wins").html(wins);
-            $(".crystal-image").remove();
-            resetGame();
-        }
-
-        else if (counter > targetNumber) {
-            losses++;
-            $("#losses").html(losses);
-            $(".crystal-image").remove();
-            resetGame();
-        }
-
-    })
-});
+startGame();
