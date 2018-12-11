d4878fa9ff43ff47f2b8a70977bf6ab84acf783c
Christopher Hein <chrishein@Christophers-MacBook-Pro.local>
2018-12-10 20:26:57 -0500

    moved numberOptions to get new numbers upon reset

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -21,56 +21,76 @@ function getRandomInts() {
     return ints;
 }
 
-var numberOptions = getRandomInts();
-console.log(numberOptions);
+var numberOptions
+
 // Next we create a for loop to create crystals for every numberOption.
-function crystalNumbers() {
+function createNewCrystals() {
     for (var i = 0; i < numberOptions.length; i++) {
+
+        // For each iteration, we will create an imageCrystal
+        var imageCrystal = $("<img>");
+
+        // First each crystal will be given the class ".crystal-image".
+        // This will allow the CSS to take effect.
+        imageCrystal.addClass("crystal-image");
+
+        // Each imageCrystal will be given a src link to the crystal image
+        imageCrystal.attr("src", "../crystal-game/assets/images/" + i + ".png");
 
         // Each imageCrystal will be given a data attribute called data-crystalValue.
         // This data attribute will be set equal to the array value.
-        $(".crystal").attr("data-crystalvalue", numberOptions[i]);
+        imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+        // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
+        $("#crystals").append(imageCrystal);
     }
 }
 
 //This function will reset the game
 function resetGame() {
-
+    numberOptions = getRandomInts();
     // This generates the random number for the score that needs to be achieved
     targetNumber = 9//getRandomInt(19, 120);
     $("#targetNumber").html(targetNumber);
-    crystalNumbers();
+    createNewCrystals();
     counter = 0;
     $("#counter").html(counter)
+    console.log(numberOptions);
+    $(document).ready();
 }
+
 resetGame();
 // This time, our click event applies to every single crystal on the page. Not just one.
-$(".crystal").on("click", function () {
+$(document).ready(function () {
+    $(".crystal-image").on("click", function () {
 
-    // Determining the crystal's value requires us to extract the value from the data attribute.
-    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
+        // Determining the crystal's value requires us to extract the value from the data attribute.
+        // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
+        // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
+        // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
 
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-    // We then add the crystalValue to the user's "counter" which is a global variable.
-    // Every click, from every crystal adds to the global counter.
-    counter += crystalValue;
+        var crystalValue = ($(this).attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue);
+        // We then add the crystalValue to the user's "counter" which is a global variable.
+        // Every click, from every crystal adds to the global counter.
+        counter += crystalValue;
 
-    // All of the same game win-lose logic applies. So the rest remains unchanged.
-    $("#counter").html(counter);
+        // All of the same game win-lose logic applies. So the rest remains unchanged.
+        $("#counter").html(counter);
 
-    if (counter === targetNumber) {
-        wins++;
-        $("#wins").html(wins);
-        resetGame();
-    }
+        if (counter === targetNumber) {
+            wins++;
+            $("#wins").html(wins);
+            $(".crystal-image").remove();
+            resetGame();
+        }
 
-    else if (counter > targetNumber) {
-        losses++;
-        $("#losses").html(losses);
-        resetGame();
-    }
+        else if (counter > targetNumber) {
+            losses++;
+            $("#losses").html(losses);
+            $(".crystal-image").remove();
+            resetGame();
+        }
 
+    })
 });
